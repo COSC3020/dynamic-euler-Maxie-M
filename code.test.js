d6007c02a95a68a68ba8bd010ed852083ee5e4ad
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-11-17 18:49:00 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,6 +1,21 @@
+// code.test.js 
+// Maxie Machado 
+// Dynamic Euler
+
 const fs = require('fs');
 const assert = require('assert');
-eval(fs.readFileSync('code.js')+'');
+eval(fs.readFileSync('code.js') + '');
 
-assert(factorial(10) === 3628800);
-assert(e(10) === 2.7182818011463845);
+assert.strictEqual((function testFactorial() {
+    let fact = 1;
+    for (let i = 1; i <= 10; i++) fact *= i;
+    return fact;
+})(), 3628800);
+
+assert.strictEqual(e(0), 1);
+assert.strictEqual(e(1), 2);
+assert.strictEqual(e(2), 2.5);
+assert.strictEqual(Math.abs(e(10) - 2.7182818011463845) < 1e-10, true);
+
+console.log("e(15):", e(15));
+assert.strictEqual(Math.abs(e(15) - 2.718281828458995) < 1e-10, true);
